6b8ebad2c60f06c78d2fed4eb4951ce61b79d094
blake S <design.harls@icloud.com>
2023-03-25 12:56:40 +0100

    init project

---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+   <header class="wrapper__header">
+
+       <h1 class="wrapper__title">Podcaster</h1>
+   </header>
+</template>
+
+<style scoped>
+</style>
