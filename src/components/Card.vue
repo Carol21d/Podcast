bd53383bff77ee6d858e1f0ba794b8189209a60c
blake S <design.harls@icloud.com>
2023-03-25 17:01:16 +0100

    added styles in card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -9,7 +9,7 @@ const props = defineProps({
 
 <template>
     <div class="wrapper">
-        <img  :src="podcast['im:image'][1].label" />
+        <img  :src="podcast['im:image'][0].label" />
         <h2>{{ podcast["im:name"].label }}</h2>
         <p>Author: {{ podcast["im:artist"].label}}</p>
 
@@ -18,3 +18,41 @@ const props = defineProps({
 
  
 </template>
+
+<style lang="scss" scoped>
+.wrapper{
+    display: flex;
+    width: 0 auto;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    border-radius: 1px solid;
+    box-shadow:  0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); 
+
+    img{
+        position: relative;
+        border-radius: 45vh;
+        width:24vh;
+        bottom: 45vh;
+        
+    }
+
+    h2{
+        position: relative;
+        font-size: 1.2em;
+        bottom: 3vh;
+
+    }
+
+
+    p{
+        position: relative;
+        font-size: 20px;
+    }
+}
+
+
+
+
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,29 +1,41 @@
 <script setup>
-import Header from '../components/Header.vue';
-import Card from '../components/Card.vue';
-import ApiPodcast from '../services/ApiCall';
-import {ref ,onBeforeMount} from 'vue';
+import Header from "../components/Header.vue";
+import Card from "../components/Card.vue";
+import ApiPodcast from "../services/ApiCall";
+import { ref, onBeforeMount } from "vue";
 
 const apiPodcast = new ApiPodcast();
 const podcasts = ref([]);
 
-onBeforeMount(async () =>{
+onBeforeMount(async () => {
   await apiPodcast.fetchAllPodcast();
   podcasts.value = apiPodcast.getPodcast();
-})
-
-
-
+});
 </script>
 
 <template>
   <main>
-    <Header/>
+    <Header />
 
     <div class="card">
-
-   <Card v-for="podcast in podcasts" :podcast="podcast" />
+      <Card v-for="podcast in podcasts" :podcast="podcast" />
     </div>
-   
   </main>
 </template>
+
+<style lang="scss" scoped>
+main{
+display: flex;
+flex-direction: column;
+margin: 0 auto;
+
+.card{
+  display: grid;
+  grid-template-columns: repeat(4,1fr);
+  gap: 2rem;
+}
+
+
+
+}
+</style>
